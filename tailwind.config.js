d7428bf77be7d165ea226aac08ae8a35c854ca52
SHIN  SEUNG A <tlstmddk0303@gmail.com>
2024-12-29 22:26:30 +0900

    폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        pretendard: ['Pretendard', 'sans-serif'], // Pretendard 폰트 추가
+        pretendard: ['Pretendard', 'sans-serif'],// Pretendard 폰트 추가
+        laundry: ['LaundryGothic', 'sans-serif'], //LaundryGothic 폰트 추가
       },
       boxShadow: {
         inset: "inset 0 0 10px",
