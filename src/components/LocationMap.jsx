7c5f31fc8b6ad89fe6599a17c4dcbf1aa79c1dfe
ohgoodgood <jskim9504@gmail.com>
2025-01-03 18:52:47 +0900

    ✨ feature: 지도 컴포넌트 완성

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -13,23 +13,38 @@ function LocationMap({ title, coordinates }) {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    if (!window.kakao || !coordinates) return;
+    console.log("Executing useEffect");
+    try {
+      if (!mapRef.current || !window.kakao || !coordinates) {
+        console.log("Missing Kakao or coordinates");
+        return;
+      }
+      console.log("Coordinates: ", coordinates);
+      console.log("Map Ref: ", mapRef.current);
 
-    const { latitude, longitude } = coordinates;
-    const map = new window.kakao.maps.Map(mapRef.current, {
-      center: new window.kakao.maps.LatLng(latitude, longitude),
-      level: 3, // 지도 확대 수준
-    });
+      const { latitude, longitude } = coordinates;
+      const map = new window.kakao.maps.Map(mapRef.current, {
+        center: new window.kakao.maps.LatLng(latitude, longitude),
+        level: 3, // 지도 확대 수준
+      });
 
-    // 마커 추가
-    new window.kakao.maps.Marker({
-      map,
-      position: new window.kakao.maps.LatLng(latitude, longitude),
-      title,
-    });
-  }, [coordinates]);
+      // 마커 추가
+      new window.kakao.maps.Marker({
+        map,
+        position: new window.kakao.maps.LatLng(latitude, longitude),
+        title,
+      });
+    } catch (error) {
+      console.error("Map rendering error: ", error);
+    }
+  });
 
-  return <div ref={mapRef} style={{ width: "100%", height: "200px" }} />;
+  return (
+    <div
+      ref={mapRef}
+      className="w-full h-[140px] rounded-lg border-solid border-[1px] border-gray-200 shadow-card-shadow"
+    />
+  );
 }
 
 export default LocationMap;
