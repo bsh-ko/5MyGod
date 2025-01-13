49051a5accc4b92f4dad46cf0aa4480278e219bc
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-13 15:28:04 +0900

    🔥

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,6 @@ const Layout = lazy(() => import("@components/layout"));
 const MainPage = lazy(() => import("@pages/board/MainPage"));
 const Detail = lazy(() => import("@pages/board/Detail"));
 const New = lazy(() => import("@pages/board/New"));
-//const MyErrand = lazy(() => import("@pages/user/MyErrand"));
 const Login = lazy(() => import("@pages/user/Login"));
 const Signup = lazy(() => import("@pages/user/Signup"));
 const MyPage = lazy(() => import("@pages/user/Mypage"));
@@ -21,7 +20,6 @@ const router = createBrowserRouter(
         { index: true, element: <MainPage /> },
         { path: "products/:_id", element: <Detail /> },
         { path: "products/new", element: <New /> },
-        // { path: "users/myerrand", element: <MyErrand /> },
         // { path: ":type", element: <List /> },
         // { path: ":type/new", element: <New /> },
         // { path: ":type/:_id/edit", element: <Edit /> },
