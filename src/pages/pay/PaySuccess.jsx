1d594070289518e3fec29384655aa3cc8249e405
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-14 13:30:27 +0900

    💄

--- src/pages/pay/PaySuccess.jsx
+++ src/pages/pay/PaySuccess.jsx
@@ -3,16 +3,15 @@ import LikeButton from "@pages/pay/LikeButton";
 export default function PaySuccess() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      {/* <img src="/src/assets/x-icon.png" /> */}
       <div className="content-center">
-        <img src="/src/assets/success.png" />
-        <p className="font-pretendard font-extrabold text-popup-title text-primary-500 text-center">
+        <img src="/src/assets/success.png" className="mx-auto" />
+        <p className="font-pretendard font-extrabold text-popup-title text-primary-500 text-center my-5">
           결제 완료
         </p>
         <p className="font-pretendard text-popup-text text-gray-700 text-center">
           20,000원을 결제했어요.
         </p>
-        <p className="font-pretendard text-popup-text text-gray-700 text-center">
+        <p className="font-pretendard text-popup-text text-gray-700 text-center mt-32 mb-2">
           종로구뽀또그래퍼님의
           <br />
           심부름에 만족하셨다면
