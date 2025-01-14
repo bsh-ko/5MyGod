9558be051993254b9a5d24eb9469852c35ddbe03
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-14 13:55:20 +0900

    💄 컴포넌트 정렬

--- src/pages/pay/PaySuccess.jsx
+++ src/pages/pay/PaySuccess.jsx
@@ -3,7 +3,7 @@ import LikeButton from "@pages/pay/LikeButton";
 export default function PaySuccess() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="content-center">
+      <div className="content-center flex flex-col items-center">
         <img src="/src/assets/success.png" className="mx-auto" />
         <p className="font-pretendard font-extrabold text-popup-title text-primary-500 text-center my-5">
           결제 완료
@@ -16,7 +16,7 @@ export default function PaySuccess() {
           <br />
           심부름에 만족하셨다면
         </p>
-        <LikeButton />
+        <LikeButton className="mx-auto" />
       </div>
     </div>
   );
