cf2ff53ba1069ac0699b5dededda72ce7fce9188
ohgoodgood <jskim9504@gmail.com>
2025-01-15 11:12:35 +0900

    Revert "Feature89/ 결제 기능"

--- src/pages/pay/Payment.jsx
+++ src/pages/pay/Payment.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
-function Payment({ productId, payAmount }) {
+function Payment({ payamount }) {
   useEffect(() => {
     const loadPortOneSDK = () => {
       const script = document.createElement("script");
@@ -22,40 +21,25 @@ function Payment({ productId, payAmount }) {
     loadPortOneSDK();
   }, []);
 
-  const navigate = useNavigate();
-  const handlePayment = async () => {
-    try {
-      if (window.PortOne) {
-        const result = await window.PortOne.requestPayment({
-          storeId: "store-e4038486-8d83-41a5-acf1-844a009e0d94",
-          paymentId: productId, //결제 ID - 심부름 고유값으로, testm5w7k로 시작하고 3자리 추가해주면 될것같습니다 ex. 1번 심부름은 testm5w7k001
-          orderName: "테스트 결제",
-          totalAmount: payAmount, //결제 금액
-          currency: "KRW",
-          channelKey: "channel-key-4ca6a942-3ee0-48fb-93ef-f4294b876d28",
-          payMethod: "CARD",
-          card: {},
-          redirectUrl: "http://localhost:5173/pay/paysuccess", //결제 후 이동할 url
-        });
-        console.log("결제 완료되었습니다.", result);
-        navigate("/pay/paysuccess");
-      } else {
-        alert("결제 모듈이 로드되지 않았습니다.");
-      }
-    } catch (error) {
-      //사용자가 결제 모듈을 취소했을 때
-      console.log("결제를 취소했습니다.", error);
+  const handlePayment = () => {
+    if (window.PortOne) {
+      window.PortOne.requestPayment({
+        storeId: "store-e4038486-8d83-41a5-acf1-844a009e0d94",
+        paymentId: "testm5w64bli",
+        orderName: "테스트 결제",
+        totalAmount: payamount,
+        currency: "KRW",
+        channelKey: "channel-key-4ca6a942-3ee0-48fb-93ef-f4294b876d28",
+        payMethod: "CARD",
+        card: {},
+        redirectUrl: "https://sdk-playground.portone.io/",
+      });
+    } else {
+      alert("결제 모듈이 로드되지 않았습니다.");
     }
   };
 
-  return (
-    <button
-      onClick={handlePayment}
-      className="w-[393px] h-16 bg-primary-500 text-white font-laundry font-bold tracking-tighter text-[24px]"
-    >
-      {payAmount}원 결제하기
-    </button>
-  );
+  return <button onClick={handlePayment}>결제하기</button>;
 }
 
 export default Payment;
