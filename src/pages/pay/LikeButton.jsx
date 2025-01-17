6243c045d5e98516ac462b450c18e184c07a797d
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-17 14:58:00 +0900

    💄 좋아요 버튼 상태와 클릭 이벤트 추가

--- src/pages/pay/LikeButton.jsx
+++ src/pages/pay/LikeButton.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
 export default function LikeButton() {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleClick = () => {
+    setIsLiked((prev) => !prev);
+  };
+
   return (
     <>
-      <div className="flex items-center justify-center w-40 h-16 rounded-[10px] bg-white border-gray-100 border-[2px] gap-1 ">
-        <span className="text-gray-700 text-popup-text">좋아요</span>
+      <div
+        onClick={handleClick}
+        className={`flex items-center justify-center w-40 h-16 rounded-[10px] gap-1 cursor-auto hover:bg-primary-300 ${
+          isLiked
+            ? "bg-primary-500 "
+            : "bg-white border-2 border-gray-100 border-solid"
+        }`}
+      >
+        <span
+          className={`text-popup-text ${
+            isLiked ? "text-white" : "text-gray-700"
+          }`}
+        >
+          좋아요
+        </span>
         <img className="h-7" src="/assets/heart.png"></img>
       </div>
     </>
