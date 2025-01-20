58bdb0bfaa13ffb0665a632cf84b99f8a516da02
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-20 11:07:49 +0900

    🎨 알람 생성을 동적으로 관리

--- src/pages/user/NotificationCreate.jsx
+++ src/pages/user/NotificationCreate.jsx
@@ -1,7 +1,26 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import PropTypes from "prop-types";
 
-export default function NotificationCreate() {
+const contentByType = (type, userName) => {
+  const contentMap = {
+    apply: `${userName}님이 지원했습니다.`,
+    comment: `${userName}님이 댓글을 작성했습니다.`,
+    expire: "게시 한 심부름의 기한이 만료됐습니다.",
+    complete: "심부름을 완료했습니다.",
+    accept: `${userName}님이 심부름 지원을 수락했습니다.`,
+  };
+
+  return contentMap[type] || "";
+};
+
+export default function NotificationCreate({
+  type,
+  targetId,
+  userName,
+  errandId,
+  errandTitle,
+}) {
   const axios = useAxiosInstance();
 
   const mutation = useMutation({
@@ -13,13 +32,13 @@ export default function NotificationCreate() {
 
   const handleButtonClick = () => {
     const notificationData = {
-      type: "apply", // apply || comment || expired
-      target_id: 4, // 알림을 받는 사람의 _id값 product?.item?.seller?._id
+      type,
+      target_id: `${targetId}`, // 알림을 받는 사람의 _id값 product?.item?.seller?._id
       channel: "toast",
-      content: "종로구뽀또그래퍼님이 지원했어요.", // `${지원자 닉네임}님이 지원했어요.`
+      content: contentByType(type, userName),
       extra: {
-        url: "/errand/2", // `/errand/${productId}`
-        errand_title: "SNS 프로필 사진 찍어주세요", //product?.item?.name
+        url: `/errand/${errandId}`,
+        errand_title: `${errandTitle}`, //product?.item?.name
       },
     };
 
@@ -29,21 +48,17 @@ export default function NotificationCreate() {
   return (
     <button
       onClick={handleButtonClick}
-      className="bg-blue-500 text-white px-4 py-2 rounded"
+      className="bg-blue-500 text-white px-4 py-2"
     >
       Send Notification
     </button>
   );
 }
 
-/*{
-    "type": "qna",
-    "target_id": 4,
-    "channel": "toast",
-    "content": "종로구뽀또그래퍼님이 지원했어요.",
-    "extra": {
-      "url": "/errand/2",
-      "errand_title": "SNS 프로필 사진 찍어주세요"
-    }
-  }
-*/
+NotificationCreate.propTypes = {
+  type: PropTypes.oneOf(["apply", "comment", "expire", "complete", "accept"]),
+  targetId: PropTypes.number.isRequired,
+  userName: PropTypes.string,
+  errandId: PropTypes.number.isRequired,
+  errandTitle: PropTypes.string.isRequired,
+};
