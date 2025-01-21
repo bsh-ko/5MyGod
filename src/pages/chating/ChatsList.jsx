35f85db5fdf44eafc781ff5735f4a7f672bca8c8
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-21 17:44:27 +0900

    🔥

--- src/pages/chating/ChatsList.jsx
+++ src/pages/chating/ChatsList.jsx
@@ -1,5 +1,4 @@
 export default function ChatsList() {
-  console.log("여기는 채팅");
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-[393px] mx-auto my-auto min-h-screen bg-background-color items-center justify-center flex">
